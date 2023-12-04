2c3525aeb421334a0168b110cba24338c626eb01
ivanrmnvch <ivansylkin@gmail.com>
2023-12-04 22:14:16 +0300

    #chore: create packages

--- src/main/java/systems_engineering_spring/SES_HW.java
+++ src/main/java/systems_engineering_spring/SES_HW.java
@@ -1,13 +1,13 @@
-package systems_engineering_spring.demo;
+package systems_engineering_spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class SES_HW {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(SES_HW.class, args);
 	}
 
 }
--- src/test/java/systems_engineering_spring/DemoApplicationTests.java
+++ src/test/java/systems_engineering_spring/DemoApplicationTests.java
@@ -1,4 +1,4 @@
-package systems_engineering_spring.demo;
+package systems_engineering_spring;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
