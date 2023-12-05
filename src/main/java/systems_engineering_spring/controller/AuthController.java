7f68aaba6a89775f9d03b3e09f37166727c52ef5
ivanrmnvch <ivansylkin@gmail.com>
2023-12-05 21:57:38 +0300

    #fix: auth controller

--- src/main/java/systems_engineering_spring/controller/AuthController.java
+++ src/main/java/systems_engineering_spring/controller/AuthController.java
@@ -1,4 +1,33 @@
 package systems_engineering_spring.controller;
 
+import lombok.RequiredArgsConstructor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import systems_engineering_spring.dto.JwtRequest;
+import systems_engineering_spring.dto.RegistrationUserDto;
+import systems_engineering_spring.dto.TokenDto;
+import systems_engineering_spring.service.AuthService;
+
+@RestController
+@RequiredArgsConstructor
+@Slf4j
 public class AuthController {
+  private final AuthService authService;
+
+  @PostMapping("/auth")
+  public ResponseEntity<?> createAuthToken(@RequestBody JwtRequest authRequest) {
+    return authService.createAuthToken(authRequest);
+  }
+
+  @PostMapping("/registration")
+  public ResponseEntity<?> createNewUser(@RequestBody RegistrationUserDto registrationUserDto) {
+    return authService.createNewUser(registrationUserDto);
+  }
+
+  @RequestMapping(value = "/refresh", method = RequestMethod.POST, produces = "application/json")
+  public ResponseEntity<?> refreshToken(@RequestBody TokenDto tokenDto) {
+    return authService.refreshToken(tokenDto);
+  }
 }
